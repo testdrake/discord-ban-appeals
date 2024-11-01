e8af2975271b0b1683a55dd82b2c16f547c9d33f
Horizon Development <111465344+testdrake@users.noreply.github.com>
2024-11-01 05:57:59 -0700

    Update submission-created.js

--- func/submission-created.js
+++ func/submission-created.js
@@ -79,23 +79,6 @@ export async function handler(event, context) {
             } catch (e) {
                 console.log(e);
             }
-
-            if (!process.env.DISABLE_UNBAN_LINK) {
-                const unbanUrl = new URL("/.netlify/functions/unban", DEPLOY_PRIME_URL);
-                const unbanInfo = {
-                    userId: userInfo.id
-                };
-    
-                message.components = [{
-                    type: 1,
-                    components: [{
-                        type: 2,
-                        style: 5,
-                        label: "Approve appeal and unban user",
-                        url: `${unbanUrl.toString()}?token=${encodeURIComponent(createJwt(unbanInfo))}`
-                    }]
-                }];
-            }
         }
 
         let result;
